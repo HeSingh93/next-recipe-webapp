066999d34bf23e745992bfd536fe0a9007a84e06
hamnord <h.nordenstein@hotmail.com>
2021-01-22 11:48:15 +0100

    fix banner styling

--- pages/YEEET.js
+++ pages/YEEET.js
@@ -1,5 +1,5 @@
 import {Container, Row, Col, Image, Nav} from 'react-bootstrap';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/mainPage.module.css';
 
 export default function YEET() {
 
@@ -8,20 +8,17 @@ export default function YEET() {
       <div className={styles.bannerImage}>
         <div className={styles.navContainer}>
       <Row className="justify-content-md-center">
-        <Nav justify variant="tabs" >
+        <Nav justify >
           <Nav.Item><Nav.Link className={styles.navItemText} href="/">HOME</Nav.Link></Nav.Item>
-          <Nav.Item><Nav.Link className={styles.navItemText}href="/">ABOUT</Nav.Link></Nav.Item>
-          <Nav.Item><Nav.Link className={styles.navItemText}href="#CONTACTS">CONTACTS</Nav.Link></Nav.Item>
+          <Nav.Item><Nav.Link className={styles.navItemText} href="/">ABOUT</Nav.Link></Nav.Item>
+          <Nav.Item><Nav.Link className={styles.navItemText} href="#CONTACTS">CONTACTS</Nav.Link></Nav.Item>
         </Nav>
       </Row>
-      <Row className="justify-content-center p-3">
-        <div className={styles.searchBarContainer}>
-        <input className={"form-control-sm "} type="text" placeholder="Search..."/>
-        </div>
+      <Row className="justify-content-md-center p-3">
+        <input className={styles.searchBar} type="search" placeholder=" Search..."/>
       </Row>
         </div>
-      </div>
     </div>
-
+    </div>
   )
 };
