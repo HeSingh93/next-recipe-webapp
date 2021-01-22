6978bef9b714097c1352c5bd25db95e785ac931e
hamnord <h.nordenstein@hotmail.com>
2021-01-22 15:06:31 +0100

    fix overflow in banner

--- pages/index.js
+++ pages/index.js
@@ -1,70 +1,44 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import {Container, Row, Col, Image, Nav, Navbar, NavDropdown} from 'react-bootstrap';
+import styles from '../styles/mainPage.module.css';
+import Logo from "../public/js/Logo";
 import FoodCard from "../components/FoodCard/FoodCard";
 
 export default function Home() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
-
-            <main className={styles.main}>
-                <h1 className={styles.title}>
-                    Welcome to <a href="https://nextjs.org">Next.js!</a>
-                </h1>
-
-                <p className={styles.description}>
-                    Get started by editing {' '}
-                    <code className={styles.code}>pages/index.js</code>
-                </p>
-
-                <div className={styles.grid}>
-                    <a href="https://nextjs.org/docs" className={styles.card}>
-                        <h3>Documentation &rarr;</h3>
-                        <p>Find in-depth information about Next.js features and API.</p>
-                    </a>
-
-                    <a href="https://nextjs.org/learn" className={styles.card}>
-                        <h3>Learn &rarr;</h3>
-                        <p>Learn about Next.js in an interactive course with quizzes!</p>
-                    </a>
-
-                    <a
-                        href="https://github.com/vercel/next.js/tree/master/examples"
-                        className={styles.card}
-                    >
-                        <h3>Examples &rarr;</h3>
-                        <p>Discover and deploy boilerplate example Next.js projects.</p>
-                    </a>
-
-                    <a
-                        href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className={styles.card}
-                    >
-                        <h3>Deploy &rarr;</h3>
-                        <p>
-                            Instantly deploy your Next.js site to a public URL with Vercel.
-                        </p>
-                    </a>
-                </div>
-            </main>
-            <FoodCard/>
-            <footer className={styles.footer}>
-                <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by{' '}
-                    <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo}/>
-                </a>
-            </footer>
-
+  return (
+      <div className={styles.indexContainer}>
+        <div className={styles.bannerContainer}>
+          <div className={styles.bannerImage}>
+            <div className={styles.navContainer}>
+              <Row className={styles.centerAlignment}><Logo/></Row>
+              <Row className={styles.centerAlignment}>
+                <Navbar collapseOnSelect expand={"md | lg | xl"}>
+                  <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                  <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav.Link className={styles.navItemText} href="/">HOME</Nav.Link>
+                    <Nav.Link className={styles.navItemText} href="/">ABOUT</Nav.Link>
+                    <Nav.Link className={styles.navItemText} href="#CONTACTS">CONTACTS</Nav.Link>
+                  </Navbar.Collapse>
+                </Navbar>
+              </Row>
+              <Row className={styles.centerAlignment}>
+                <input className={styles.searchBar} type="search" placeholder=" Search..."/>
+              </Row>
+            </div>
+          </div>
         </div>
-    )
+        <FoodCard/>
+        <footer className={styles.footer}>
+          <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Powered by{' '}
+            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo}/>
+          </a>
+        </footer>
+
+      </div>
+  )
 }
